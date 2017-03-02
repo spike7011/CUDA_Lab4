cb430f51b53c1629bb89526dbb779f7d7c07f160
spike7011 <jasonglass2017@u.northwestern.edu>
2017-03-01 20:46:42 -0600

    "lets try"

--- scan_largearray_kernel.cu
+++ scan_largearray_kernel.cu
@@ -10,9 +10,10 @@
 #define LOG_NUM_BANKS 5
 // Lab4: You can use any other block size you wish.
 #define BLOCK_SIZE 256
+#define DEFAULT_NUM_ELEMENTS 16000000
 
 // Lab4: Host Helper Functions (allocate your own data structure...)
-float AllocateDeviceArray(float * A)
+float* AllocateDeviceArray(float * A)
 {
 	float * Adevice = A;
 	int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
@@ -28,58 +29,56 @@ float AllocateDeviceArray(float * A)
 void CopyToDeviceArray(float * Adevice, float * Ahost)
 {
 	int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
-	cudaMemcpy(Adevice, Ahost, size,
-		cudaMemcpyHostToDevice);
-	}
+	cudaMemcpy(Adevice, Ahost, size,cudaMemcpyHostToDevice);
+}
 
-	// Copy a device matrix to a host matrix.
-	void CopyFromDeviceArray(float * Ahost, float * Adevice)
-	{
-		int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
-		cudaMemcpy(Ahost, Adevice, size,
-			cudaMemcpyDeviceToHost);
-		}
+// Copy a device matrix to a host matrix.
+void CopyFromDeviceArray(float * Ahost, float * Adevice)
+{
+	int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
+	cudaMemcpy(Ahost, Adevice, size, cudaMemcpyDeviceToHost);
+}
 
-		// Lab4: Device Functions
+// Lab4: Device Functions
 
 
-		// Lab4: Kernel Functions
-		__global__ void computeKernel( float* reference, float* idata, const unsigned int len)
-		{
-			// reference[0] = 0;
-			// double total_sum = 0;
-			// for( unsigned int i = 1; i < len; ++i)
-			// {
-			//     total_sum += idata[i-1];
-			//     reference[i] = idata[i-1] + reference[i-1];
-			// }
-			// if (total_sum != reference[len-1])
-			//     printf("Warning: exceeding single-precision accuracy.  Scan will be inaccurate.\n");
+// Lab4: Kernel Functions
+__global__ void computeKernel( float* reference, float* idata, const unsigned int len)
+{
+	// reference[0] = 0;
+	// double total_sum = 0;
+	// for( unsigned int i = 1; i < len; ++i)
+	// {
+	//     total_sum += idata[i-1];
+	//     reference[i] = idata[i-1] + reference[i-1];
+	// }
+	// if (total_sum != reference[len-1])
+	//     printf("Warning: exceeding single-precision accuracy.  Scan will be inaccurate.\n");
 
-		}
+}
 
 
 
-		// **===-------- Lab4: Modify the body of this function -----------===**
-		// You may need to make multiple kernel calls, make your own kernel
-		// function in this file, and then call them from here.
-		void prescanArray(float *outArray, float *inArray, int numElements)
-		{
+// **===-------- Lab4: Modify the body of this function -----------===**
+// You may need to make multiple kernel calls, make your own kernel
+// function in this file, and then call them from here.
+void prescanArray(float *outArray, float *inArray, int numElements)
+{
 
-			int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
-			float * Adevice_in = AllocateDeviceArray(inArray);
-			CopyToDeviceArray(Adevice_in, inArray, size);
-			float * Adevice_out = AllocateDeviceArray(outArray);
-			CopyToDeviceArray(Adevice_out, outArray, size);
+	int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
+	float * Adevice_in = AllocateDeviceArray(inArray);
+	CopyToDeviceArray(Adevice_in, inArray, size);
+	float * Adevice_out = AllocateDeviceArray(outArray);
+	CopyToDeviceArray(Adevice_out, outArray, size);
 
-			dim3 dimGrid(DEFAULT_NUM_ELEMENTS/BLOCK_SIZE,1);
-			dim3 dimBlock(16,16);
+	dim3 dimGrid(DEFAULT_NUM_ELEMENTS/BLOCK_SIZE,1);
+	dim3 dimBlock(16,16);
 
-			computeKernel << dimGrid, dimBlock >>> (Adevice_out,Adevice_in, DEFAULT_NUM_ELEMENTS);
-			cudaThreadSynchronize();
-			CopyFromDeviceArray(outArray, Adevice_out);
-		}
-		// **===-----------------------------------------------------------===**
+	computeKernel << dimGrid, dimBlock >>> (Adevice_out ,Adevice_in, DEFAULT_NUM_ELEMENTS);
+	cudaThreadSynchronize();
+	CopyFromDeviceArray(outArray, Adevice_out);
+}
+// **===-----------------------------------------------------------===**
 
 
-		#endif // _PRESCAN_CU_
+#endif // _PRESCAN_CU_
