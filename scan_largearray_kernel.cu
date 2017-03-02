6bea7d697ccd9a6d065ceb67badb8b23b688df62
spike7011 <jasonglass2017@u.northwestern.edu>
2017-03-01 20:41:57 -0600

    "algorithm second commit"

--- scan_largearray_kernel.cu
+++ scan_largearray_kernel.cu
@@ -3,6 +3,7 @@
 
 // includes, kernels
 #include <assert.h>
+#include <scan_largearray.cu>
 
 
 #define NUM_BANKS 32
@@ -11,56 +12,74 @@
 #define BLOCK_SIZE 256
 
 // Lab4: Host Helper Functions (allocate your own data structure...)
-
-
-// Lab4: Device Functions
-
-
-// Lab4: Kernel Functions
-
-
-// **===-------- Lab4: Modify the body of this function -----------===**
-// You may need to make multiple kernel calls, make your own kernel
-// function in this file, and then call them from here.
-void prescanArray(float *outArray, float *inArray, int numElements)
+Matrix AllocateDeviceArray(const float * A)
 {
-
-	extern __shared__ float temp[];// allocated on invocation
-	int thid = threadIdx.x;
-	int offset = 1;
-	temp[2*thid] = inArray[2*thid]; // load input into shared memory
-	temp[2*thid+1] = inArray[2*thid+1];
-	for (int d = numElements>>1; d > 0; d >>= 1) // build sum in place up the tree
-	{
-		__syncthreads();
-		if (thid < d)
-		{
-			int ai = offset*(2*thid+1)-1;
-			int bi = offset*(2*thid+2)-1;
-			temp[bi] += temp[ai];
-		}
-		offset *= 2;
-	}
-	if (thid == 0) { temp[numElements - 1] = 0; } // clear the last element
-	for (int d = 1; d < numElements; d *= 2) // traverse down tree & build scan
-	{
-		offset >>= 1;
-		__syncthreads();
-		if (thid < d)
-		{
-			int ai = offset*(2*thid+1)-1;
-			int bi = offset*(2*thid+2)-1;
-			float t = temp[ai];
-			temp[ai] = temp[bi];
-			temp[bi] += t;
-		}
-	}
-	__syncthreads();
-	outArray[2*thid] = temp[2*thid]; // write results to device memory
-	outArray[2*thid+1] = temp[2*thid+1];
-
+	float * Adevice = A;
+	int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
+	cudaMalloc((void**)&Adevice, size);
+	return Adevice;
 }
-// **===-----------------------------------------------------------===**
+
+// Allocate a matrix of dimensions height*width
+//	If init == 0, initialize to all zeroes.
+//	If init == 1, perform random initialization.
+
+// Copy a host matrix to a device matrix.
+void CopyToDeviceArray(float * Adevice, const float * Ahost)
+{
+	int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
+	cudaMemcpy(Adevice, Ahost, size,
+		cudaMemcpyHostToDevice);
+	}
+
+	// Copy a device matrix to a host matrix.
+	void CopyFromDeviceArray(float * Ahost, const float * Adevice)
+	{
+		int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
+		cudaMemcpy(Ahost, Adevice, size,
+			cudaMemcpyDeviceToHost);
+		}
+
+		// Lab4: Device Functions
 
 
-#endif // _PRESCAN_CU_
+		// Lab4: Kernel Functions
+		__global__ void computeKernel( float* reference, float* idata, const unsigned int len)
+		{
+			// reference[0] = 0;
+			// double total_sum = 0;
+			// for( unsigned int i = 1; i < len; ++i)
+			// {
+			//     total_sum += idata[i-1];
+			//     reference[i] = idata[i-1] + reference[i-1];
+			// }
+			// if (total_sum != reference[len-1])
+			//     printf("Warning: exceeding single-precision accuracy.  Scan will be inaccurate.\n");
+
+		}
+
+
+
+		// **===-------- Lab4: Modify the body of this function -----------===**
+		// You may need to make multiple kernel calls, make your own kernel
+		// function in this file, and then call them from here.
+		void prescanArray(float *outArray, float *inArray, int numElements)
+		{
+
+			int size = DEFAULT_NUM_ELEMENTS * sizeof(float);
+			float * Adevice_in = AllocateDeviceArray(inArray);
+			CopyToDeviceArray(Adevice_in, inArray, size);
+			float * Adevice_out = AllocateDeviceArray(outArray);
+			CopyToDeviceArray(Adevice_out, outArray, size);
+
+			dim3 dimGrid(DEFAULT_NUM_ELEMENTS/BLOCK_SIZE,1);
+			dim3 dimBlock(16,16);
+
+			computeKernel << dimGrid, dimBlock >>> (Adevice_out,Adevice_in, DEFAULT_NUM_ELEMENTS);
+			cudaThreadSynchronize();
+			CopyFromDeviceArray(outArray, Adevice_out);
+		}
+		// **===-----------------------------------------------------------===**
+
+
+		#endif // _PRESCAN_CU_
